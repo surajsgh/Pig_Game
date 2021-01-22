d6be993a7316320084689b23315b37e5246667a8
Suraj Singh <surajsingh220698@outlook.com>
2021-01-23 00:40:49 +0530

    Do reset the scores back to zeros.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// Selecting Elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
